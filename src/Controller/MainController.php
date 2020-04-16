b3141e2d0ce47fb7c965c18d345101b042ecfde8
oussamajomaa <osm70@gmx.com>
2020-04-16 10:10:02 +0200

    login auth registration

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,12 +8,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/main", name="main")
+     * @Route("/", name="main")
      */
     public function index()
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
+        return $this->render('main/index.html.twig');
+    }
+
+    /**
+     * @Route("/back", name="back")
+     */
+    public function start()
+    {
+        return $this->render('base.html.twig');
     }
 }
